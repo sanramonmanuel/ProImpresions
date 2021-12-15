f888a42f81260772ff8c36fe768da9425653dc7d
manuelsanramon <manuel@strategicvisionz.com>
2021-12-15 15:07:18 +0800

    new marc ui

---
+++ tab layout function.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.tabs').tabs();
+    $('.tab-1').appendTo('#Pest Sightings & Conducive Conditions');
+    $('.tab-2').appendTo('#Notes');
+    $('.tab-3').appendTo('#General Information');
+    $('.tab-4').appendTo('#Other Documents');
+});
