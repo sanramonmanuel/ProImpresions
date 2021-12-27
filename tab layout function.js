353c291e9a47425b0d06f77b3587bffe6b6ea433
manuelsanramon <manuel@strategicvisionz.com>
2021-12-28 00:40:05 +0800

    tabs ui v2

--- tab layout function.js
+++ tab layout function.js
@@ -1,8 +1,13 @@
+// TAB JS - Copy and paste the following into the Custom Footer Code section
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+// Find all the class name from each component and add JavaScript to the page builder
+// Use the Class names you've assigned to each component and append them to TAB IDs you've created
 $(document).ready(function(){
     $('.tabs').tabs();
-    $('.tab-1').appendTo('#Pest Sightings & Conducive Conditions');
-    $('.tab-2').appendTo('#Notes');
-    $('.tab-3').appendTo('#General Information');
-    $('.tab-4').appendTo('#Other Documents');
-    $('.tab-5').appendTo('#Approved Materials');
+    $('.tab-1').appendTo('#pestSightings');
+    $('.tab-2').appendTo('#notes');
+    $('.tab-3').appendTo('#genInfo');
+    $('.tab-4').appendTo('#otherDocs');
+    $('.tab-5').appendTo('#approvedMats');
 });
